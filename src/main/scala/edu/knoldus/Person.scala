cc125628518b02b1d2a417bbe91aa0755f0d6a3e
Manjot Kaur <manjotmona@gmail.com>
2018-01-22 01:26:46 +0530

    Final Changes

--- src/main/scala/edu/knoldus/Person.scala
+++ src/main/scala/edu/knoldus/Person.scala
@@ -8,9 +8,10 @@ import java.io.{File, PrintWriter}
 
 import scala.io.Source
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.apache.log4j.Logger
+
 
 @SerialVersionUID(15)
 class Address(val street: String, val houseNumber: Int)
@@ -21,23 +22,34 @@ class Person(val name: String,
     @transient val day: String,
     val address: Address,
     val salary: Float,
-    @transient val luckyNumber: Int)
+    @transient val luckyNumber: Int) {
+  def display: String = {
+    this.name + " " + this.age + " " + this.day + " " + this.address.houseNumber + " " +
+    this.address.street + " " + this.salary + " " + this.luckyNumber
+  }
+}
 
 object Application extends App {
   val log = Logger.getLogger(this.getClass)
   val address = new Address("Prashant Vihar", 87)
   val person = new Person("Mona", 12, "Friday", address, 12000, 12)
+  print(person.display + "\n")
+
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
   val out = mapper.writeValueAsString(person)
-  log.info("****This is the serialised JSON object**** \n\n\n")
+  log.info("****This is the serialised JSON object**** \n")
   log.info(out)
-  log.info("\n\n\n****Writing object in File**** \n\n\n")
+  log.info("\n****Writing object in File**** \n")
   val pw = new PrintWriter(new File("File.txt"))
   pw.write(out)
   pw.close()
-  log.info("****Reading object from File****\n\n\n")
-  Source.fromFile("File.txt").foreach { x => print(x) }
+  log.info("****Reading object from File****\n")
+  val str = Source.fromFile("File.txt").mkString
+  log.info("\n")
+  val in = mapper.readValue(str, classOf[Person])
+  log.info(in.display)
 }
 
 
